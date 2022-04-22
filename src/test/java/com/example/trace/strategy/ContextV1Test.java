5a17fee6f41e176d6c3186d232d3dc818fc905d1
DolphaGo <adamdoha@naver.com>
2022-04-23 03:40:16 +0900

    전략 패턴 - 시작

---
+++ src/test/java/com/example/trace/strategy/ContextV1Test.java
@@ -0,0 +1,35 @@
+package com.example.trace.strategy;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        final long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        final long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+}
