e73817bc3bfec99bc44642f61797b5eb94a9a191
DolphaGo <adamdoha@naver.com>
2022-04-25 02:13:42 +0900

    쓰레드 로컬 동기화 - 적용

--- src/main/java/com/example/LogTraceConfig.java
+++ src/main/java/com/example/LogTraceConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.trace.logtrace.FieldLogTrace;
 import com.example.trace.logtrace.LogTrace;
+import com.example.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+//        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
