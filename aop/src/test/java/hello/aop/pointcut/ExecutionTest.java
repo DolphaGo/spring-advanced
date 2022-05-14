46c3a1b3bbf418890ca0d69b9ae5dc7bf1aa3d28
DolphaGo <adamdoha@naver.com>
2022-05-15 03:48:32 +0900

    execution -1

--- aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -1,8 +1,11 @@
 package hello.aop.pointcut;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -24,5 +27,83 @@ public class ExecutionTest {
     void printMethod() {
         // helloMethod=public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
         log.info("helloMethod={}", helloMethod);
+    }
+
+    @DisplayName("가장 정확한 포인트 컷")
+    @Test
+    void exactMatch() {
+        // helloMethod=public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("가장 많이 생략한 포인트 컷")
+    @Test
+    void allMatch() {
+        pointcut.setExpression("execution(* *(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("이름만 매치하는 포인트컷")
+    @Test
+    void nameMatch() {
+        pointcut.setExpression("execution(* hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("패턴 네임 매칭 포인트컷")
+    @Test
+    void patternMatch() {
+        pointcut.setExpression("execution(* hel*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("패턴 네임 매칭 포인트컷")
+    @Test
+    void patternMatch2() {
+        pointcut.setExpression("execution(* *el*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("패턴 네임 매칭 포인트컷 실패하는 경우")
+    @Test
+    void nameMatchFalse() {
+        pointcut.setExpression("execution(* nono(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @DisplayName("패키지 매칭 정확한 포인트컷")
+    @Test
+    void packageMatch() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("패키지 매칭에서 생략 가능한 형태")
+    @Test
+    void packageMatch2() {
+        pointcut.setExpression("execution(* hello.aop.member.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("패키지 매칭이 실패하는 경우")
+    @Test
+    void packageMatch3() {
+        pointcut.setExpression("execution(* hello.aop.*.*(..))"); // member Package인지 지정하지 않았기 때문에 false가 나온다.
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @DisplayName("서브 패키지 매칭")
+    @Test
+    void packageMatchSubPackage1() {
+        pointcut.setExpression("execution(* hello.aop.member..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("서브 패키지 매칭")
+    @Test
+    void packageMatchSubPackage2() {
+        pointcut.setExpression("execution(* hello.aop..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 }
--- md/스프링 AOP_포인트 컷.md
+++ md/스프링 AOP_포인트 컷.md
@@ -129,10 +129,86 @@ public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.Strin
 
 ## excecution - 1
 
+```
+execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern) throws-pattern?)
+
+execution(접근제어자? 반환타입 선언타입?메서드이름(파라미터) 예외?)
+```
+
+- 메소드 실행 조인 포인트를 매칭한다.
+- `?`는 생략할 수 있다.
+- `*`같은 패턴을 지정할 수 있다.
+
+```java
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void printMethod() {
+        // helloMethod=public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        log.info("helloMethod={}", helloMethod);
+    }
+
+    @Test
+    void exactMatch() {
+        // helloMethod=public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+}
+```
+
+- `AspectJExpressionPointcut` 에 `pointcut.setExpression` 을 통해서 포인트컷 표현식을 적용할 수 있다.
+
+- `pointcut.matches(메서드,대상 클래스`를 실행하면 지정한 포인트컷 표현식의 매칭여부를 `true`, `false` 로 반환한다.
+
+> 매칭 조건
+
+- 접근제어자?: public
+- 반환타입: String
+- 선언타입?: hello.aop.member.MemberServiceImpl
+- 메서드이름: hello
+- 파라미터: (String)
+- 예외?: 생략
+
+`MemberServiceImpl.hello(String)` 메서드와 포인트컷 표현식의 모든 내용이 정확하게 일치한다. 
+따라서 `true` 를 반환한다.
+
+> 가장 많이 생략한 포인트 컷 형태
+
+```java
+@DisplayName("가장 많이 생략한 포인트 컷")
+@Test
+void allMatch() {
+    pointcut.setExpression("execution(* *(..))");
+    assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+}
+```
+
+> 매칭 조건
+
+- 접근제어자?: 생략 
+- 반환타입: * 
+- 선언타입?: 생략 
+- 메서드이름: * 
+- 파라미터: (..)
+- 예외?: 없음
+
+`*` 은 아무 값이 들어와도 된다는 뜻이다.
+
+파라미터에서 `..` 은 파라미터의 타입과 파라미터 수가 상관없다는 뜻이다.
+`( 0..* )` 파라미터는 뒤에 자세히 정리하겠다.
 ## excecution - 2
 
 ## within
-
 ## args
 
 ## @target, @within
