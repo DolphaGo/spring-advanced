43789d7864a36eed53dd8ff68155a3b9a7e9e418
DolphaGo <adamdoha@naver.com>
2022-05-25 07:04:58 +0900

    프록시 기술과 한계 - 의존관계 주입

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,31 @@
+package hello.aop.proxyvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Import(ProxyDIAspect.class)
+@SpringBootTest(properties = "spring.aop.proxy-target-class=true")
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService; // 인터페이스로 주입
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl; // 구체 타입으로 주입
+
+    @Test
+    void go() {
+        log.info("memberService class = {}", memberService.getClass());
+        memberService.hello("hello");
+        log.info("memberServiceImpl class = {}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
